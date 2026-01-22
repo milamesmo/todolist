57056c3799a3a83c93b2f1e304c2cb04af1135fa
milamesmo <camilaabegg.n@gmail.com>
2026-01-22 19:34:18 -0300

    chore: initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+function saveToLocalStorage() {
+  localStorage.setItem("toDoList", JSON.stringify(toDoList));
+}
+
+rendertoDoList();
+
+function rendertoDoList() {
+  let list = document.querySelector(".list");
+  list.innerHTML = "";
+  for (let i = 0; i < toDoList.length; i++) {
+    let toDoItem = toDoList[i];
+    const { name, date } = toDoItem;
+    list.innerHTML += `
+    
+    <div class="task__name">${name}</div>
+    <div class="task__date">${date}</div>
+         <button class="delete__btn" onclick="
+      removeItem(${i});
+      
+      ">Delete</button>
+    `;
+  }
+}
+
+function send(event) {
+  if (event.key === "Enter") {
+    addItem();
+  }
+}
+
+function addItem() {
+  const inputElement = document.querySelector(".userItem");
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector(".userDate");
+  const date = dateInputElement.value;
+
+  toDoList.push({
+    name,
+    date
+  });
+  saveToLocalStorage();
+  rendertoDoList();
+  inputElement.value = "";
+}
+
+function removeItem(index) {
+  toDoList.splice(index, 1);
+  saveToLocalStorage();
+  rendertoDoList();
+}
